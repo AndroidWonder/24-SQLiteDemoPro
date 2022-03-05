0253dedda987b596246e4d5fcd0a94da0f9064b3
Android Wonder <jim.pepe@verizon.net>
2022-03-05 08:24:27 -0500

    add comments

--- app/src/main/java/com/course/example/sqlitedemopro/Animal.java
+++ app/src/main/java/com/course/example/sqlitedemopro/Animal.java
@@ -5,7 +5,8 @@ public class Animal {
 	private int id;
 	private String name;
 	private int quantity;
-	
+
+	//accessors and mutators
 	public int getId() {
 		return id;
 	}
@@ -24,7 +25,8 @@ public class Animal {
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-	
+
+	//constructors
 	public Animal(String name, int quantity) {
 		super();
 		this.name = name;
